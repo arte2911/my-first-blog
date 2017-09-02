7a87b15b79c2dcd404064ca6d76fa2929f53d8f2
carolina <ceam2911@hotmail.com>
2017-09-02 14:58:46 -0300

    mi primera app Djsngo

---
+++ python_intro.py
@@ -0,0 +1,12 @@
+nombres = ["Daniela", "Cristian", "Gabriel", "Paulina", "Emilia", "Diego", "Javier", "Angela"]
+def saludos (lista):
+	for e in lista:
+		print ("Hola "+e.upper())
+saludos (nombres)
+saludos (["Jose", "Juan" ])
+
+print (range (1,6))
+for i in range (1,6):
+	print (i)
+	
+	
